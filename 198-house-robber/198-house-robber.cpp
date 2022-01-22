e331a2de556198efdc937d57f1f51ab936a4237e
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-01-22 18:46:30 +0530

    Time: 3 ms (43.02%), Space: 7.8 MB (42.21%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    
-    // vector<int>hash;
-    unordered_map<int,int>hash;
-    int dp(vector<int> nums,int i){
-        
-        if(i==0) return nums[0];
-        if(i==1) return max(nums[0],nums[1]);
-        
-        if(hash.find(i) == hash.end()){
-            hash[i] = max(dp(nums,i-1),dp(nums,i-2) + nums[i]);
-        }
-        return hash[i];
-        
-    }
-    
     int rob(vector<int>& nums) {
-        return dp(nums,nums.size()-1);
+        int n = nums.size();
+        vector<int>hash(n+1);
+        if(n==1) return nums[0]; 
+        hash[0] = nums[0];
+        hash[1] = max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            hash[i] = max(hash[i-1],nums[i]+hash[i-2]);
+        }
+        return hash[n-1];
+        
+        
     }
 };
