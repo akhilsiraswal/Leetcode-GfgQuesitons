a87d90715f87f1e8a4eab7ef6f41eac9171226c9
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-02-05 14:18:24 +0530

    Time: 59 ms (39.57%), Space: 89.3 MB (86.42%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Naive Solution..
-        int n1=nums1.size(),n2=nums2.size(),nums3[n1+n2],i=0,j=0,k=0;
-        
-        while(i<n1 || j<n2){
-            if(i<n1 && j<n2 ){
-                if(nums1[i]<nums2[j]){
-                    nums3[k++]=nums1[i++];
-                }else{
-                    nums3[k++]=nums2[j++];
+        if(nums2.size()<nums1.size()) return findMedianSortedArrays(nums2,nums1);
+        int n1=nums1.size(),n2=nums2.size(),low=0,high=n1;
+        while(low<=high){
+            int cut1 = (low+high)/2;
+            int cut2 =  (n1+n2+1)/2 - cut1;
+            
+            int left1 = cut1==0?INT_MIN:nums1[cut1-1];
+            int left2 = cut2==0?INT_MIN:nums2[cut2-1];
+            int right1 = cut1>=n1?INT_MAX:nums1[cut1];
+            int right2 = cut2>=n2?INT_MAX:nums2[cut2];
+            
+            
+            if(left1<=right2 && left2<=right1) {
+                if((n1+n2)%2==0){
+                    // return left1;
+                    return (max(left1,left2) + min(right1,right2)) / 2.0;
+                }else {
+                    // return left1;
+                    return max(left1,left2);
                 }
-            }else if(i<n1) nums3[k++]=nums1[i++];
-            else if(j<n2) nums3[k++]=nums2[j++];
+            }else if(left1>right2){
+                high=cut1-1;
+            }else {
+                low = cut1+1;
+            }
+            
+            
+            
         }
-        
-        int n3=n1+n2;
-        if(n3%2==0){
-            int n=n3/2;
-            return (double)(nums3[n]+nums3[n-1])/2;
-        }else {
-            return nums3[n3/2];
-        }
-        
-        
-        
+        return 0.0;
     }
 };
