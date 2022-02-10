c9888a47d066d47632e87ad792541de464c605ea
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-02-10 14:24:16 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (30.72%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
         
-        unordered_map<int,int>mp;
+       sort(ranges.begin(),ranges.end());
         for(int i=0;i<ranges.size();i++){
-           int r1=ranges[i][0],r2=ranges[i][1];
-            for(int i=r1;i<=r2;i++) mp[i]=true;
+            if(left>=ranges[i][0] && left<=ranges[i][1]) left = ranges[i][1]+1;
+            
+            if(left>right) return true;
         }
-        
-        for(int i=left;i<=right;i++){
-            if(mp[i]!=true) return false;
-        }
-        
-        
-        
-       
-        return true;
+        return false;
     }
 };
