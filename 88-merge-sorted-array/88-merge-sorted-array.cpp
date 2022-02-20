cba360a1935a690aedb38452723747cde05a158d
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-02-20 13:47:51 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (72.83%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-     vector<int>temp;
+        vector<int>result;
         int i=0,j=0;
         while(i<m && j<n){
-            if(nums1[i]<=nums2[j])temp.push_back(nums1[i++]);
-            else temp.push_back(nums2[j++]);
+            if(nums1[i]<=nums2[j]){
+                result.push_back(nums1[i++]);
+            }else{
+                result.push_back(nums2[j++]);
+            }
         }
-        while(i<m) temp.push_back(nums1[i++]);
-        while(j<n) temp.push_back(nums2[j++]);
+        
+        while(i<m) result.push_back(nums1[i++]);
+        while(j<n) result.push_back(nums2[j++]);
+        
+        for(int i=0;i<nums1.size();i++) nums1[i] = result[i];
         
         
-        for(int i=0;i<temp.size();i++) nums1[i] = temp[i];
+       
     }
 };
