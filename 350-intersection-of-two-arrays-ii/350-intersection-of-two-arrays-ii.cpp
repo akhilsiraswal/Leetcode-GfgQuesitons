7670aea1b6a8eff3cfe0ba81d472d58c2c71238a
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-02-20 13:52:05 +0530

    Time: 4 ms (88.45%), Space: 10.7 MB (30.34%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>result;
+        unordered_map<int,int>mp;
+        for(auto i:nums1)mp[i]++;
+        for(int i=0;i<nums2.size();i++){
+            if(mp[nums2[i]]>0){
+                result.push_back(nums2[i]);
+                mp[nums2[i]]--;
+            }
+        }
+        return result;
+    }
+};
