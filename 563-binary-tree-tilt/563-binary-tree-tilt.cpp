df6a133c8da22349ff8e98a2c8957edad16a74c5
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-03-03 23:37:33 +0530

    Time: 18 ms (80.48%), Space: 23.7 MB (80.18%) - LeetHub

--- 563-binary-tree-tilt/563-binary-tree-tilt.cpp
+++ 563-binary-tree-tilt/563-binary-tree-tilt.cpp
@@ -14,14 +14,19 @@ public:
     
     int sum(TreeNode*root){
         if(!root) return 0;
-        return sum(root->left)+sum(root->right)+root->val;
+        int lsum = sum(root->left);
+        int rsum = sum(root->right);
+        result += abs(lsum-rsum);
+        return lsum+rsum+root->val;
+        // return sum(root->left)+sum(root->right)+root->val;
     }
     int result=0;
     int findTilt(TreeNode* root) {
-        if(!root) return result;
-        result+=abs(sum(root->left)-sum(root->right));
-        findTilt(root->left);
-        findTilt(root->right);
+        if(!root) return 0;
+        // result+=abs(sum(root->left)-sum(root->right));
+        // findTilt(root->left);
+        // findTilt(root->right);
+            sum(root);
         return result;
     }
 };
