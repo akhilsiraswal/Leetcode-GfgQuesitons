67eeab61b5edfcf34b1a7a5caea34f9229f23558
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-03-04 16:59:04 +0530

    Time: 6 ms (25.45%), Space: 7.6 MB (12.16%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    unordered_map<int,int>mp;
-    int count(int idx,string s){
-        int n = s.size();
-        if(idx == n) return 1;
+    // unordered_map<int,int>mp;
+    int count(int idx,string s,vector<int>&dp){
+        int n=s.length();
+        if(dp[idx]>-1) return dp[idx];
         
-        if(s[idx] == '0')return 0;
-        int result;
-        if(mp[idx+1]!=0){
-            result = mp[idx+1];
-        }else{
-            mp[idx+1] = count(idx+1,s);
-            result = mp[idx+1];
+        if(s[idx] == '0') {
+            dp[idx] = 0;
+            return 0;
+            
         }
+        int result = count(idx+1,s,dp);
+        
         if(idx<n-1 && (s[idx] == '1' || (s[idx] == '2' && s[idx+1] <'7' ))) {
-            if(mp[idx+2]!=0) result+=mp[idx+2];
-            else{
-                mp[idx+2] = count(idx+2,s);
-                result +=mp[idx+2];
-            }
-            // result +=count(idx+2,s);
+            result+=count(idx+2,s,dp);
         }
+        return dp[idx] = result;
         
-        return result;
+        
+        
+        
         
     }
     
     int numDecodings(string s) {
-        int res = count(0,s);
+        vector<int>dp(s.length()+1,-1);
+        dp[s.length()] =1;
+        int res = count(0,s,dp);
         return res;
     }
 };
