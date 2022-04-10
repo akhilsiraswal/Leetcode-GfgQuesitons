61676c9e4872efbed901e5393121b3174faa13cc
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-04-10 16:08:38 +0530

    Time: 4 ms (44.08%), Space: 5.9 MB (82.49%) - LeetHub

---
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    int helper(long long  n,long long  count){
+        if(n==1) return count;
+        
+        if(n%2==0) return helper(n/2,count+1);
+        else{
+            count = min(helper(n-1,count+1),helper(n+1,count+1));
+        }
+        return count;
+    }
+    
+    int integerReplacement(int n) {
+        return helper(n,0);
+    }
+};
