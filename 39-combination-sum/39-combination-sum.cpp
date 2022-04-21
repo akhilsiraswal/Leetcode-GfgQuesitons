35e3d53c4614e52ae9fa3f942dd9656f0cf33119
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-04-21 16:54:41 +0530

    Time: 4 ms (91.61%), Space: 10.7 MB (96.55%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     vector<vector<int>>result;
     void BruteForce(vector<int>candidates,vector<int>nums,int index,int sum,int target){
         if(sum>=target){
@@ -20,6 +21,32 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int>nums;
         BruteForce(candidates,nums,0,0,target);
+        return result;
+    }
+    */
+    
+    void bruteforce(vector<int> &candidates, int target,vector<vector<int>> &result,vector<int> &temp,int idx){
+        
+            if(target <=0) {
+                if(target == 0) result.push_back(temp);
+                return;
+            }
+        
+     
+         for(int i=idx;i<candidates.size();i++){
+             temp.push_back(candidates[i]);
+             bruteforce(candidates,target-candidates[i],result,temp,i);
+             temp.pop_back();
+         }
+        return;
+        
+        
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>  result;
+        vector<int> temp;
+        bruteforce(candidates,target,result,temp,0);
         return result;
     }
 };
