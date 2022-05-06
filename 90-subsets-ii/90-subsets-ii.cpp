39df20693f45cf0f786011fcf97967cf81a856d1
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-05-06 10:29:51 +0530

    Time: 14 ms (15.32%), Space: 8.2 MB (40.39%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    
+    
+//    using bitmask method..
+    /*
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         
         int n=nums.size(),sz = pow(2,n);
@@ -26,4 +30,31 @@ public:
         
         
     }
+    */
+  
+    // using backtracking.. 
+    
+    vector<vector<int>> res;
+    void subset( vector<int> &nums,int idx,vector<int> v){
+        sort(v.begin(),v.end());
+        if(find(res.begin(),res.end(),v) == res.end())res.push_back(v);
+        for(int i=idx;i<nums.size();i++){
+            v.push_back(nums[i]);
+            
+            subset(nums,i+1,v);
+            v.pop_back();
+            
+        }
+        
+        return ;
+        
+        
+    }
+    
+     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+         vector<int> v;
+          subset(nums,0,v);
+         return res;
+         
+     }
 };
