5699986ad9ff67073ef4235ed8d694df442e610b
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-05-11 06:38:23 +0530

    Time: 130 ms (6.48%), Space: 6 MB (34.92%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -5,6 +5,7 @@ public:
     int helper(vector<char> &ch,int n,int i){
         if(n==0) return 1;
         int ans=0;
+        
         for(int j=i;j<ch.size();j++){
             ans+=helper(ch,n-1,j);
         }
