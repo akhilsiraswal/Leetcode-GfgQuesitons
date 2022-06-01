bfd641ebfdd590b2bb1823f4b0963ddf13b9833d
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-06-01 19:42:34 +0530

    Time: 319 ms (19.57%), Space: 81.5 MB (40.55%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,39 +1,47 @@
-class Solution {
+ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-       
-        int n = heights.size();
-        
-        vector<int>left(n),right(n);
-        stack<int>st;
-        for(int i=0;i<n;i++){
-            if(st.empty()) {
-                left[i] = 0;
-                st.push(i);
-            }else {
-                while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-                left[i] = st.empty()?0:st.top()+1;
-                st.push(i);
-            }
-        }
-        
-       while(!st.empty()) st.pop();
+        int n=heights.size();
+        stack<int> stl,str;
+        vector<int> l(n),r(n);
         
         for(int i=n-1;i>=0;i--){
-            if(st.empty()) {
-                right[i] = n-1;
-                st.push(i);
+            
+            if(str.empty()){
+
+                r[i] = n-1;
             }else{
-                while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-                right[i] = st.empty()?n-1:st.top()-1;
-                st.push(i);
+                
+                while(!str.empty() && heights[str.top()] >= heights[i] ) str.pop();
+                r[i] = str.empty()?n-1:str.top()-1;
             }
+            
+                str.push(i);
         }
-        int result=0;
+        
         for(int i=0;i<n;i++){
-            int l= right[i]-left[i]+1;
-            result = max(result,l*heights[i]);
+            
+            if(stl.empty()){
+                l[i] = 0;
+            }else{
+                
+                while(!stl.empty() && heights[stl.top()] >= heights[i] ) stl.pop();
+                l[i] = stl.empty()?0:stl.top()+1;
+            }
+                stl.push(i);
+            
         }
-        return result;
+        
+        int res=0;
+        
+        for(int i=0;i<n;i++){
+            res = max( res, (r[i]-l[i]+1)*heights[i] );
+            
+        }
+        return res;
+        
+        
+        
+        
     }
 };
