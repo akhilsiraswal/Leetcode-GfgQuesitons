14be06b01d60868802befd588bab4affe6df9ce9
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-06-01 16:57:46 +0530

    Time: 8 ms (63.94%), Space: 8.9 MB (31.40%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int> mp;
         for(int i=0;i<nums2.size();i++){
@@ -21,4 +22,40 @@ public:
         
         return res;
     }
+    */
+     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+         vector<int> ans,temp(nums2.size());
+         
+         stack<int> st;
+         for(int i=nums2.size()-1;i>=0;i--){
+             if(st.empty()){
+                 st.push(nums2[i]);
+                 temp[i] = -1;
+                 
+             }else{
+                 while( !st.empty()&&  st.top() < nums2[i] ) st.pop();
+                 if(st.empty()) 
+                     temp[i] = -1;
+                 else temp[i] = st.top();
+                 
+                 st.push(nums2[i]);
+             }
+         }
+         
+         unordered_map<int,int> mp;
+         for(int i=0;i<nums2.size();i++){
+             mp[nums2[i]] = temp[i];
+         }
+         
+         for(int i=0;i<nums1.size();i++){
+             ans.push_back(mp[nums1[i]]);
+         }
+         
+         // for(auto i:temp) cout<<i<<" ";
+         return ans;
+                 
+
+         
+         
+     }
 };
