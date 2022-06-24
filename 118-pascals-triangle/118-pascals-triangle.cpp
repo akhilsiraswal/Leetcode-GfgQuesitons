afab9120719f3319883a205f300ba154ed1be787
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-06-24 19:34:05 +0530

    Time: 4 ms (24.18%), Space: 6.6 MB (32.31%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     vector<vector<int>> generate(int numRows) {
         vector<int>temp;
         vector<vector<int>>result;
@@ -28,4 +29,25 @@ public:
         
         return result;
     }
+    */
+     vector<vector<int>> generate(int numRows) {
+         vector<vector<int>> res;
+         if(numRows==0) return res;
+         res.push_back({1});
+         if(numRows == 1) return res;
+         res.push_back({1,1});
+         if(numRows == 2) return res;
+         
+         for(int i=2;i<numRows;i++){
+             vector<int> temp;
+             for(int j=0;j<=i;j++){
+                 if(j==0 || j== i) temp.push_back(1);
+                else {
+                    temp.push_back(res[i-1][j-1]+res[i-1][j]);
+                }
+             }
+             res.push_back(temp);
+         }
+         return res;
+     }
 };
