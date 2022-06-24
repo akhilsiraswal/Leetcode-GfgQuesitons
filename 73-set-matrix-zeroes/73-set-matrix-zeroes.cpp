a07a4b389f977cb46046921bc9cf1034dd6819eb
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-06-24 19:21:22 +0530

    Time: 31 ms (16.96%), Space: 13.3 MB (53.26%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -30,6 +30,7 @@ public:
     }
     */
     
+    /*
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
@@ -54,4 +55,29 @@ public:
         }
         
     }
+    */
+     void setZeroes(vector<vector<int>>& matrix) {
+         int val = 1e9;
+         for(int i=0;i<matrix.size();i++){
+
+             for(int j=0;j<matrix[i].size();j++){
+                 if(matrix[i][j] == 0 ){
+                     for(int k=0;k<matrix.size();k++){
+                         if(matrix[k][j]!=0)matrix[k][j] = val;
+                     }
+                     for(int k=0;k<matrix[i].size();k++){
+                         if(matrix[i][k]!=0)matrix[i][k] = val;
+                     }
+                 }
+             }
+         }
+         
+         for(int i=0;i<matrix.size();i++){
+             for(int j=0;j<matrix[i].size();j++){
+                 if(matrix[i][j] == val) matrix[i][j] = 0;
+             }
+         }
+         
+         
+     }
 };
