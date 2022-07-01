6006efef496a3854cd5076ef316ab07a7021b43c
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-07-01 18:48:31 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.11%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     void rotate(vector<vector<int>>& matrix) {
         reverse(matrix.begin(),matrix.end());
         
@@ -9,6 +10,19 @@ public:
                 
             }
         }
+        
+    }
+    
+    */
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(),matrix.end());
+        
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i;j<matrix[i].size();j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
         
     }
 };
