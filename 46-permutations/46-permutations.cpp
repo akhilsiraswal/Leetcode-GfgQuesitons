944eee9595ddf2e4f08419ac65780e6250753ff9
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-07-01 18:05:39 +0530

    Time: 10 ms (18.39%), Space: 8.8 MB (8.37%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -28,6 +28,7 @@ public:
 
 class Solution {
 public:
+    /*
     vector<vector<int>>result;
     
     void BruteForce(vector<int>perms,vector<int>nums, vector<bool>visited){
@@ -54,5 +55,31 @@ public:
         BruteForce(perms,nums,visited);
         return result;
         
+    }
+    */
+    
+    vector<vector<int>> res;
+    void bruteforce(vector<int> &nums,vector<int> num,vector<bool> visited){
+        if(num.size() == nums.size() ) {
+
+            res.push_back(num);
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            if(visited[i]) continue;
+            num.push_back(nums[i]);
+            visited[i] = true;
+            bruteforce(nums,num,visited);
+            visited[i] = false;
+            num.pop_back();
+        }
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> num;
+        vector<bool> visited(nums.size(),false);
+        bruteforce(nums,num,visited);
+        return res;
     }
 };
