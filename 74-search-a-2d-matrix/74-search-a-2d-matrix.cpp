eff07e9ce2643d691cea23436deb553a0de8bf46
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-07-03 10:35:05 +0530

    Time: 4 ms (77.24%), Space: 9.5 MB (51.72%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -20,6 +20,7 @@ public:
     }
     
     */
+    /*
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
     
         int m = matrix.size(),n=matrix[0].size();
@@ -36,4 +37,18 @@ public:
         }
         return false;
     }
+    */
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    
+    int i=0,j=matrix[0].size()-1;
+    while(i<matrix.size() && j>=0){
+        if(matrix[i][j] == target) return true;
+        else if (matrix[i][j]<target) i++;
+        else j--;
+    }
+    return false;
+    
+}
+    
 };
