f2bc9c5c8722726e808b144492b3d16458852b0b
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-07-04 18:12:06 +0530

    Time: 3 ms (43.82%), Space: 6.5 MB (44.34%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int path(int i,int j,int m,int n,vector<vector<int>> &dp){
         if(i>=m||j>=n) return 0;
         if(i==m-1&& j==n-1) {
@@ -26,5 +27,21 @@ public:
         return path(0,0,m,n,visited);
         
          
+    }
+    */
+    
+    
+    int bruteforce(int m,int n,int i,int j,vector<vector<int>> &mp){
+        if(i>m || j>n) return 0;
+        if(i == m && j == n){
+            return 1;
+        }
+        if(mp[i][j]) return mp[i][j];
+        return mp[i][j] = bruteforce(m,n,i,j+1,mp)+bruteforce(m,n,i+1,j , mp);
+    }
+    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> mp(m+1,vector<int>(n+1,0));
+        return bruteforce(m,n,1,1,mp);
     }
 };
