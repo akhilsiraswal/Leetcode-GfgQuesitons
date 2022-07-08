93a7c20aafd6665811123b278de3742b4c87afad
akhilsiraswal <50474250+akhilsiraswal@users.noreply.github.com>
2022-07-08 13:48:16 +0530

    Time: 116 ms (69.37%), Space: 19.9 MB (96.48%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /*
+    
     vector<vector<int>> threeSum(vector<int>& nums) {
         int l=0,n=nums.size();
         sort(nums.begin(),nums.end());
@@ -40,4 +42,98 @@ public:
         
         
     }
+    */
+    
+    /*
+     vector<int> twoSum(vector<int>& nums, int target,int l,int h) {
+         unordered_map<int,int>temp;
+         
+         for(int i=l;i<h;i++){
+             temp[nums[i]] = i;
+         }
+        // sort(nums.begin(),nums.end());         
+         
+         while(l<h){
+             if(nums[l]+nums[h] == target){
+                 return {nums[l],nums[h]};
+             }else if(nums[l]+nums[h]>target)h--;
+             else l++;
+         }
+         return {};
+     }
+    
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        
+        vector<vector<int>> res;
+        if(n<3) return res;
+        set<vector<int>> st;
+        
+        for(int i=0;i<n;i++){
+            if(i!=0 && nums[i-1] == nums[i]) continue;
+            int target = 0-nums[i];
+            int j=n-1;
+            while(j>i){
+                if(j>i && nums[j+1]==nums[j]) {
+                    j--;
+                    continue;
+                }
+                vector<int> val;
+                val = twoSum(nums,target,i+1,j);
+                if(val.size()!=0){
+                    val.push_back(nums[i]);
+                    sort(val.begin(),val.end());
+                    st.insert(val);
+                }
+                j--;
+                
+            }
+        }
+        for(auto i:st)res.push_back(i);
+        
+        return res;
+        
+        
+        
+    }
+    */
+    
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        
+        vector<vector<int>> result;
+        for(int i=0;i<n;i++){
+            if(i == 0 || (i>0 && nums[i-1]!=nums[i])){
+                int l=i+1,r=n-1;
+                int num = 0-nums[i];
+                while(l<r){
+                    if(nums[l]+nums[r] == num){
+                        result.push_back({nums[l],nums[i],nums[r]});
+                        
+                        while(l<r && nums[l] == nums[l+1]) l++;
+                        while(l<r && nums[r] == nums[r-1]) r--;
+                        l++;
+                        r--;
+                        
+                    }else{
+                        if(nums[l]+nums[r] > num) r--;
+                        else l++;
+                    }
+
+                }
+            }
+            
+        }
+        
+        // for(auto i:st) result.push_back(i);
+        return result;
+        
+        
+        
+    }
+    
+    
 };
